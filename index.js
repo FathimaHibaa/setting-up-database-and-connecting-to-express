dc5d2822a65320fb4146638b32087d9d18b14106
FathimaHibaa <fathimahiba1243@gmail.com>
2025-10-04 13:19:54 +0530

    Completed database connection assignment

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+require('dotenv').config()
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
-const port = 3010;
+app.use = (express.json());
 
-app.use(express.static('static'));
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/inventoryDB';
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+connect.mongoose(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+.then(()=> {
+  console.log('Connected to MongoDB');
+})
+
+.catch((err)=>{
+  console.log(`Error connecting to MongoDB: ${err}`);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
